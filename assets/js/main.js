9800459e83ed6e765a9035b8acdf5e94bc28b20c
superyaooo <superyaooo@gmail.com>
2017-07-14 00:33:52 -0500

    update back-to-top button to stay above footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,19 @@
-// back to top
+// back to top button
 $(window).scroll(function() {
     if ($(this).scrollTop() >= 300) {
-        $('#back-to-top').fadeIn(200);
+        $('#back-to-top').fadeIn("slow");
+
+        var footerToTop = $(document).height() - $('footer').height();
+        if($(this).scrollTop() + $(this).height() < footerToTop) {
+            $('#back-to-top').css('bottom', '1em');
+            $('#back-to-top').css('position', 'fixed');
+        } else {
+            $('#back-to-top').css('bottom', '75px');
+            $('#back-to-top').css('position', 'absolute');
+        }
+
     } else {
-        $('#back-to-top').fadeOut(200);
+        $('#back-to-top').fadeOut(250);
     }
 });
 $('#back-to-top').click(function() {
